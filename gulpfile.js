f61de504d41ead4b6cbeeded0f260f3eea131021
Yuraima Estevez <yuraima.estevez@tommiecopper.com>
2015-12-16 09:50:43 -0500

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+// gulpfile.js =-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+var gulp     = require('gulp');
+var sass     = require('gulp-sass');
+var concat   = require('gulp-concat');
+var uglify   = require('gulp-uglify');
+
+gulp.task('styles', function() {
+  gulp.src('./src/sass/styles.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./dist/css/'))
+});
+
+// watch files for live reload
+gulp.task('watch', function() {
+    gulp.watch('app/index.html', ['html']);
+    gulp.watch('src/sass/**/*.scss', ['styles']);
+});
+
+gulp.task('default', ['watch', 'styles']);  // took out 'open', 'connect'
