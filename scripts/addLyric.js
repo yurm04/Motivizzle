57fd943bfbbaa28d7a7548964148697bcfd0a90f
Tim Shea <timshea@Tims-MacBook-Air.local>
2016-01-04 19:24:11 -0500

    added some lyrics to get this going

--- scripts/addLyric.js
+++ scripts/addLyric.js
@@ -12,10 +12,21 @@ and then close database connection through callback
 connectDb(addLyrics);
 
 function addLyrics() {
-    motiviz.addLyric("You've got enemies?  Good, that means you actually stood up for something", 'Eminem', closeConnection);
-    motiviz.addLyric("Stop lookin' at what you ain't got and start being thankful for what you do got", 'T.I', closeConnection);
-    motiviz.addLyric("Some of y'all are not where you want to be in life.  Yet you party every weekend. What exactly are you celebrating?", 'T.I', closeConnection);
+    motiviz.addLyric("We can’t change the world until we change ourselves.", 'Biggie Smalls', closeConnection);
+    motiviz.addLyric("You have to keep your vision clear, cause only a coward lives in fear.", 'Nas', closeConnection);   
+    motiviz.addLyric("Living life is a choice. Making a difference in someone else’s isn’t", 'Kid Cudi', closeConnection);
+    motiviz.addLyric("Damn right I like the life I live, cause I went from negative to positive.", 'Biggie Smalls', closeConnection);
+    motiviz.addLyric("Life is a crazy ride and nothing is guaranteed.", 'Eminem', closeConnection);   
+    motiviz.addLyric("Living well eliminates the need for revenge.", 'Kanye West', closeConnection);
+    motiviz.addLyric("If you’re scared to take chances, you’ll never have the answers.", 'Nas', closeConnection);
+    motiviz.addLyric("Forget yesterday, live for today. Tomorrow will take care of itself.", 'Rick Ross', closeConnection);
     motiviz.addLyric("We can't change the world until we change ourselves", 'Biggie Smalls', closeConnection);
+    motiviz.addLyric("If you admire someone, you should go ahead and tell them. People never get the flowers while they can still smell them.", 'Kanye West', closeConnection);
+    motiviz.addLyric("No matter where life takes me. Find me with a smile.", 'Mac Miller', closeConnection);
+    motiviz.addLyric("When people treat you like nothing, you begin to feel like nothing.", 'Drake', closeConnection);
+    motiviz.addLyric("You can make something of your life, it just depends on your drive.", 'Eminem', closeConnection);
+    motiviz.addLyric("Never apologize for what you feel. It’s like apologizing for being real.", 'Lil Wayne', closeConnection);
+    motiviz.addLyric("Regardless of how it goes down, life goes on.", 'Rick Ross', closeConnection);
 }
 
 function connectDb(cb) {
