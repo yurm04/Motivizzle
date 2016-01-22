0442bc87b49c044262e847542899b4d0972d26d6
Yuraima Estevez <yuraima.estevez@tommiecopper.com>
2016-01-22 07:30:02 -0500

    Adds token authorization check

--- src/routes/quote.js
+++ src/routes/quote.js
@@ -1,17 +1,22 @@
 // quote.js ===========================
 
 var motiviz = require('../../modules/motiviz.js');
+var SLACK_TOKEN = process.env.SLACK_TOKEN;
 
 
 // gets a random quote from db and sends it back
 module.exports.getRandom = function(req, res) {
+    if (req.query.token !== SLACK_TOKEN) {
+        return res.send('You ain\'t authorized.');
+    }
+
     motiviz.getRandomQuote(function(err, message) {
         if (err) {
-            res.send('Could not motivate you');
+            return res.send('Could not motivate you');
         }
 
         if (message) {
-            res.send(message);
+            return res.send(message);
         }
     });
 };
